12e1b00cfd6b1d68232f9b76f0f090d5697a72bc
= <subinayp108@gmail.com>
2024-07-13 21:43:27 +0530

    backend jai hok

--- backend/firebase-config.js
+++
@@ -1,9 +0,0 @@
-var admin = require("firebase-admin");
-
-var serviceAccount = require("./maitytv-642f4-firebase-adminsdk-6c0ep-1005814778.json");
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
-
-module.exports = admin;
